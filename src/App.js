2e1eaa4384e3d10f505c7c495ef4d1f84d44b2b8
Rui En <neoruien123@gmail.com>
2021-05-11 00:43:03 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import Webcam from "react-webcam";
+import "./App.css";
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(async () => {
+    const net = await cocossd.load();
+    setInterval(() => {detect(net)}, 10);
+  });
+
+  const detect = async (net) => {
+    // Check if webcam video is available
+    if (
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Configure dimensions
+      webcamRef.current.video.width = 640;
+      webcamRef.current.video.height = 480;
+      canvasRef.current.width = 640;
+      canvasRef.current.height = 480;
+      // Start detections
+      const video = webcamRef.current.video;
+      const obj = await net.detect(video);
+      // Draw bounding box
+      const ctx = canvasRef.current.getContext("2d");
+      drawBbox(obj, ctx); 
+    }
+  };
+
+  const drawBbox = (detections, ctx) =>{
+    detections.forEach(prediction => {
+      // Extract boxes and classes
+      const [x, y, width, height] = prediction['bbox']; 
+      const text = prediction['class']; 
+      // Set styling
+      const color = Math.floor(Math.random()*18000000).toString(16);
+      ctx.strokeStyle = '#' + color;
+      ctx.font = '20px Roboto';
+      // Draw annotated bbox
+      ctx.beginPath();
+      ctx.rect(x, y, width, height);
+      ctx.stroke();
+      ctx.fillStyle = '#' + color;
+      ctx.fillText(text, x, y);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p style={{
+        position: "absolute",
+        top: "8%",
+        fontSize: "3.5rem"
+      }}>
+        COCOSSD Object Detector
+      </p>
+      <p style={{
+        position: "absolute",
+        top: "18%",
+        fontSize: "2rem"
+      }}>
+        with Facecam
+      </p>
+      <div className="display-frame">
+        <Webcam ref={webcamRef} muted={true} />
+      </div>
+      <div className="display-frame">
+        <canvas ref={canvasRef} />
+      </div>
     </div>
   );
 }
